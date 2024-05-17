90c9b8c00374acc2c8a65d77cc6536cf7f7be53f
Rutvik Samant <57898632+5216225@users.noreply.github.com>
2024-05-17 12:49:45 +0100

    Modified MVC pattern code. Aligned login and reigster buttons on right side.

--- emazon/Program.cs
+++ emazon/Program.cs
@@ -1,7 +1,21 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add authentication services.
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+})
+.AddCookie(options =>
+{
+    options.LoginPath = "/Authentication/Login"; // Specify the login path
+});
 
 var app = builder.Build();
 
@@ -9,7 +23,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -18,8 +31,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+// Use authentication middleware.
+app.UseAuthentication();
+
 app.UseAuthorization();
 
+// Map default controller route.
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
