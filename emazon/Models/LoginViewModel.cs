01f2be0ddf7f144d32f770a01b958f7de9b7093f
Rutvik Samant <57898632+5216225@users.noreply.github.com>
2024-05-18 09:23:29 +0100

    Declared null state for login model

--- emazon/Models/LoginViewModel.cs
+++ emazon/Models/LoginViewModel.cs
@@ -2,7 +2,7 @@ namespace emazon.Models
 {
     public class LoginViewModel
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
     }
 }
